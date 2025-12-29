ae86a852110b2400ef85f251e4640f599b37c63a
Ivan Yong Pak Theng <ivanyong07@gmail.com>
2025-12-29 13:19:49 +0800

    Keep only main.py, ignore other files

--- bugtest.py
+++
@@ -1,8 +0,0 @@
-
-file_path_register_coaches = "coaches_acc.txt"
-
-with open(file_path_register_coaches, 'r') as f:
-    for line in f.readlines():
-        data = line.strip().split(" | ")
-        print(data[0])
-        print(data[1])
